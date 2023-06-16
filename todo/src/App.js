d1c08ca45d4b02235cc7782aba11d2c62b9fdef4
amelamelame <amelshab2018@gmail.com>
2023-06-16 16:40:04 +0530

    second ccommit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,61 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import { useState,useEffect } from 'react';
 
-export default function () {
+
+
+export default function App() {
+  const [todos, setTodo] = useState([]);
+  
+
+  useEffect(() => {
+    getData();
+
+  },[]);
+  
+  
+  const [check,setCheck] = useState(true);
+  
+  
+  const getData =async () => {
+    try {
+      const incomingData = await fetch (
+        "https://jsonplaceholder.typicode.com/"
+      );
+
+      const formattedData = await incomingData.json();
+      setTodo(formattedData);
+      console.log(formattedData);
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <h1>
-        usereffect
-      </h1>
+      <Container className = "my-5">
+     <h1 className="my-5 text-center display-2">Todo List</h1>
+      <Table striped bordered hover>
+      <thead>
+      </thead>
+      <tbody>
+        
+        {todos.map((todo,i) => (
+          <tr key={todo.id}>
+            <td>{i+1}</td>
+            <td >{todo.title}</td>
+            {
+              /*<td>todo.completed</td>*/
+            }
+          </tr>
+        ))}
+        </tbody>
+    </Table>
+ </Container>
     </div>
   )
 }
+
+        
+
---
+++ todo/src/form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+
+export default function form() {
+  return (
+    <div>form</div>
+  )
+}
